7785194e01a0e1e137ff054c5af083f0e6548d47
Ian Arellano <ian.g.arellano01@gmail.com>
2021-10-18 10:50:36 -0500

    refactored pizza-order to be correct

--- src/main/java/PizzaOrderServlet.java
+++ src/main/java/PizzaOrderServlet.java
@@ -6,8 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 
-@WebServlet("/pizza-order")
+@WebServlet(name = "PizzaServlet", urlPatterns = "/pizza-order")
 public class PizzaOrderServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("pizza-order.jsp").forward(req, resp);
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
