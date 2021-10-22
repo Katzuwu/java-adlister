f5a126a40dc438732ffbe3efa590e5327db4127d
Ian Arellano <ian.g.arellano01@gmail.com>
2021-10-22 16:53:39 -0500

    80% done with the exercise. confused on the loginservlet todo's

---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,67 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+	private Connection connection;
+
+	public MySQLUsersDao(Config config) {
+		try {
+			DriverManager.registerDriver(new Driver());
+			connection = DriverManager.getConnection(
+					config.getUrl(),
+					config.getUser(),
+					config.getPassword()
+			);
+		} catch (SQLException e) {
+			throw new RuntimeException("Error connecting to the database!", e);
+		}
+	}
+
+	@Override
+	public User findByUsername(String username) {
+		User resultedUser = null;
+		try {
+			String prepareQuery = "SELECT * FROM users WHERE username LIKE ?";
+			String searchTermWithWildcards = "%" + username + "%";
+			PreparedStatement stmt = connection.prepareStatement(prepareQuery);
+			stmt.setString(1, searchTermWithWildcards);
+
+			ResultSet rs = stmt.executeQuery();
+			resultedUser = extractUser(rs);
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		}
+		return resultedUser;
+	}
+
+	private User extractUser(ResultSet rs) throws SQLException {
+		return new User(
+				rs.getLong("id"),
+				rs.getString("username"),
+				rs.getString("email"),
+				rs.getString("password")
+		);
+	}
+
+	@Override
+	public Long insert(User user) {
+		try {
+			String insertQuery = "INSERT INTO users(username, email, password) VALUE (?, ?, ?)";
+			PreparedStatement statement = connection.prepareStatement(insertQuery, Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, user.getUsername());
+			statement.setString(2, user.getEmail());
+			statement.setString(3, user.getPassword());
+			statement.executeUpdate();
+			ResultSet rs = statement.getGeneratedKeys();
+			rs.next();
+			return rs.getLong(1);
+		} catch (SQLException throwables) {
+			throw new RuntimeException("Error creating a new ad.", throwables);
+		}
+	}
+}
---
+++ src/main/webapp/WEB-INF/register.jsp
@@ -0,0 +1,35 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ianarellano
+  Date: 10/22/21
+  Time: 4:08 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+	<jsp:include page="/WEB-INF/partials/head.jsp">
+		<jsp:param name="title" value="Register Here" />
+	</jsp:include>
+</head>
+<body>
+<jsp:include page="/WEB-INF/partials/navbar.jsp" />
+<div>
+	<h1>Register:</h1>
+	<form method="post" action="/register">
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input id="email" name="email" class="form-control" type="text">
+		</div>
+		<div class="form-group">
+			<label for="username">Username</label>
+			<input id="username" name="username" class="form-control" type="text">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input id="password" name="password" class="form-control" type="password">
+		</div>
+	</form>
+</div>
+</body>
+</html>
